290270567236a97f6a5a583b461fe54c7e259925
Nicolas <41578265@dev.azure.com>
2021-04-06 18:30:32 -0300

    Agregado de nuevas funcionalidades v1.1

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const classNames = {
     TODO_DELETE: 'todo-delete',
   }
   
-  
   const list = document.getElementById('todo-list')
   const itemCountSpan = document.getElementById('item-count')
   const uncheckedCountSpan = document.getElementById('unchecked-count')
   const contadorItems = new Contador(0);
   const contadorChecks = new Contador(0);
+  let contadorInputs = 0;
 
   function Contador(numero){
     this.contador = numero;
@@ -19,37 +19,43 @@ const classNames = {
     this.decrementar = function(){this.contador--;}
   }
 
-  function completarLista(tarea){
-    const elementoHtml = document.createElement("li")
-    const elementoInput = document.createElement("input")
-
-    elementoHtml.textContent = tarea;
-    list.appendChild(elementoHtml)
-
-    elementoInput.setAttribute("type", "checkbox")
-    elementoInput.setAttribute("onClick", "descontarCheck()");
-    elementoHtml.prepend(elementoInput)
-  }
-
-  function completarContadores(cItems, cChecks){
-    cItems.incrementar();
-    cChecks.incrementar();
-
-    itemCountSpan.textContent = cItems.contador;
-    uncheckedCountSpan.textContent = cChecks.contador;
-  }
-
-  function descontarCheck(){
-    contadorChecks.decrementar();
+  function completarContadores(){
+    contadorItems.incrementar()
+    contadorChecks.incrementar()
+    itemCountSpan.textContent = contadorItems.contador;
     uncheckedCountSpan.textContent = contadorChecks.contador;
   }
 
+  function completarInputs(elementoInput, contadorInputs){
+    elementoInput.setAttribute("type", "checkbox")
+    elementoInput.setAttribute("id", `input-value ${contadorInputs}`)
+    elementoInput.addEventListener('click', function(){
+      if(elementoInput.checked){
+        contadorChecks.decrementar()
+        uncheckedCountSpan.textContent = contadorChecks.contador;
+      }else{
+        contadorChecks.incrementar()
+        uncheckedCountSpan.textContent = contadorChecks.contador;
+      }
+    })
+  }
+
+  function completarLista(tarea){
+    contadorInputs++;
+    const elementoLista = document.createElement("li")
+    const elementoInput = document.createElement("input")
+
+    elementoLista.textContent = tarea;
+    list.appendChild(elementoLista)
+
+    completarInputs(elementoInput,contadorInputs);
+
+    elementoLista.prepend(elementoInput)
+  }
 
   function addTodo() {
-    //alert('Boton Add TODO clickeado!')
-
     let tarea = prompt("Que tarea deseas agregar?");
 
     completarLista(tarea);
-    completarContadores(contadorItems, contadorChecks);
+    completarContadores();
   }
